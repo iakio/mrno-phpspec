a71036ce52bba14af13ba9740a1907bc7cfbe052
ISHIDA Akio <ishida@coderium.com>
2014-03-08 14:48:06 +0900

    キャッシュを実装 (pass)

--- src/TimedCache.php
+++ src/TimedCache.php
@@ -3,6 +3,7 @@
 class TimedCache
 {
     protected $loader;
+    protected $cache = [];
 
     public function __construct(Loader $loader)
     {
@@ -11,6 +12,11 @@ class TimedCache
 
     public function lookup($key)
     {
-        return $this->loader->load($key);
+        if (array_key_exists($key, $this->cache)) {
+            return $this->cache[$key];
+        }
+        $value = $this->loader->load($key);
+        $this->cache[$key] = $value;
+        return $value;
     }
 }
